d661c1e09df045567fab7913a332ff566714f509
Mayank <47733256+rathoremayank@users.noreply.github.com>
2020-05-26 19:31:44 +0530

    Add files via upload

---
+++ dbscript.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE [IF NOT EXISTS] registration;
+
+use registration; 
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `username` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
